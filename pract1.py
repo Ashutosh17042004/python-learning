c2ab6c586045f108b0d02d58de0142ee8065a9de
ashutosh <ashutoshkumarsinghh17a@gmail.com>
2025-07-13 21:58:24 +0530

    Practice set of Datatypes and variables

---
+++ pract1.py
@@ -0,0 +1,24 @@
+# variables and datatypes practice set
+
+# QUES 1 WRITE A PROGRAM TO ADD TO NUMBER.
+a=int(input("Enter first no. : "))
+b=int(input("Enter second no. : "))
+print("SUM of no. is = ",a+b)
+
+# # QUES 2 FIND REMAINDER WHEN NO. IS DIVIDED BY ANY NO.
+c=int(input("Enter first no. : "))
+d=int(input("Enter the no. which divides A no. : "))
+print("remainder = ",c%d)
+
+# # QUES 3 CHECK THE TYPE TO VARIBLES 
+st=input("enter any thing :")
+print(type(st))
+
+# QUES 4 FIND AVERAGE OF TWO NO.
+num1=int(input("enter first no. : " ))
+num2=int(input("enter second no. : " ))
+print("average = ", (num1+num2)/2)
+
+# QUES 5 FIND SQUARE OF USER ENTERED NO.
+num3=int(input("enter no. : "))
+print("Square = ", num3**2)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,3 @@
-import pyttsx3
-# engine =pyttsx3.init()
-# engine.say('''hey ashu what's up''')
-# engine.runAndWait()
+print(type(a))
+# print(type(b))
+# print(type(d))
