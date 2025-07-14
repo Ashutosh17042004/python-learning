6c06d1068ef0c992d3f7d32456011385abb9f9ca
ashutosh <ashutoshkumarsinghh17a@gmail.com>
2025-07-14 22:05:48 +0530

    LEARN STRING / LIST AND TUPLES

---
+++ pract2.py
@@ -0,0 +1,25 @@
+# Strings practice set
+
+# display user enterd name by input () followed by goodafternoon
+# str =input("Enter your name : ")
+# print("GOOD AFTERNOON ",str.capitalize())
+# print("GOOD AFTERNOON ",str.upper())
+# print(f"GOOD AFTERNOON {str.upper()}")
+
+#ques 3 fill letter templates
+# name=input("enter name : ")
+# date=input(" enter date : ")
+# letter= f'''
+# Dear {name},
+# You are selected.
+# {date}
+# '''
+# print(letter)
+
+#detect double space in string
+# string=input("enter string : ")
+# print(string.find("  "))
+
+#replace in string
+string="hello  ashu  how"
+print(string.replace("  "," "))
---
+++ pract3.py
@@ -0,0 +1,22 @@
+# LIST AND TUPLES PRACTICE SET
+
+# ADD FOOD ITEMS ENTERED BY USER
+# frt_list=[]
+# f1=input("enter fruit :")
+# f2=input("enter fruit :")
+# f3=input("enter fruit :")
+# f4=input("enter fruit :")
+# frt_list.extend((f1,f2,f3,f4))
+# print(frt_list)
+
+
+# display marks of student input by user in sorted manner
+# markslist=[]
+# stud1=input("Student 1 marks")
+# stud2=input("Student 2 marks")
+# stud3=input("Student 3 marks")
+# stud4=input("Student 4 marks")
+# stud5=input("Student 5 marks")
+# markslist.extend((stud1,stud2,stud3,stud4,stud5))
+# markslist.sort()
+# print(markslist)
