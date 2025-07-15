a884a9c1bae35977abceb4c60323a2766985e43a
ashutosh <ashutoshkumarsinghh17a@gmail.com>
2025-07-15 16:37:32 +0530

    Do a practice set of "dictionary" and "set"

---
+++ pract4.py
@@ -0,0 +1,41 @@
+# "DICTIONARY" AND "SETS"  PRACTICE SET
+
+# ques 1 make dictiionary of hindi words as keys and meaning in english as valus and give option to useer to look up!!
+# dict={
+#     "chai" :"tea",
+#     "patti" : "leaves",
+#     "billi" : "cat"
+
+
+# }
+# word=input("enter word : ")
+# print(word,"meaning in english is",dict[word])
+
+#   QUES 2 write a prgram to input 8 numbers from the user and display all unique numbers(once)
+
+# s= set()
+# n1=int(input("enter no. 1 : "))
+# n2=int(input("enter no. 2 : "))
+# n3=int(input("enter no. 3 : "))
+# n4=int(input("enter no. 4 : "))
+# n5=int(input("enter no. 5 : "))
+# n6=int(input("enter no. 6 : "))
+# n7=int(input("enter no. 7 : "))
+# n8=int(input("enter no. 8 : "))
+# s.update((n1,n2,n3,n4,n5,n6,n7,n8))
+# print(s)
+
+# create an empty dict and key =friends name and values are their corresponding favorite values ,print dict
+# dict={}
+# f1=input("name frend1 : ")
+# l1=input("favorite lang. : ")
+# f2=input("name frend2 : ")
+# l2=input("favorite lang. : ")
+# f3=input("name frend3 : ")
+# l3=input("favorite lang. : ")
+# f4=input("name frend4 : ")
+# l4=input("favorite lang. : ")
+
+# dict.update({f1:l1,f2:l2,f3:l3,f4:l4})
+# print(dict)
+
