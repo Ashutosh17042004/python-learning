b2d0ddc823025fa5c09f54f2016624eae57e11f5
ashutosh <ashutoshkumarsinghh17a@gmail.com>
2025-07-15 18:11:07 +0530

    practice of conditional expression

---
+++ pract5.py
@@ -0,0 +1,43 @@
+# conditonal expressions practice set
+
+# ques 1  write a program to find greatest no. in 4 input values
+# without using if statement
+# a,b,c,d=int(input("1 st : ")),int(input("2 st : ")),int(input("3 st : ")),int(input("4 st : "))
+# list=[]
+# list.extend([a,b,c,d])
+# list.reverse()
+# print(list[0])
+
+# using if/else statement
+# if(a>b or a>c or a>d):
+#  print(f"a is greater ")
+# 
+# elif(b>a or b>c or b>d):
+#  print(f"a is greater ")
+# 
+# elif(c>a or c>b or c>d):
+#  print(f"a is greater ")
+# 
+# else:
+#     print(f"d is greater")
+# 
+
+# ques 2 take 3 subject marks and find weather student pass or fail Condition(total=40% each subj=33%)
+# m1,m2,m3=float(input("enter marks")) ,float(input("enter marks")) ,float(input("enter marks")) 
+# if m1<33 or m2<33 or m3<33 or (((m1+m2+m3)*100)/300)<40:
+#     print("Result : Fail")
+# else:
+#     print("Result : Pass")
+# print("Total pecentage",((m1+m2+m3)*100)/300)
+
+# spam message if text containing following  text
+# t1,t2,t3="make more money","subscribe now","click this"
+# maintxt="ashu is "
+# if (t1 in maintxt)  or (t2 in maintxt) or( t3 in maintxt) :
+#     print("spam text found !!!!")
+# else :
+#     print("clear") 
+
+
+
+
