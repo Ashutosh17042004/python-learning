ee7d1f9bf2cf35488c3d012a92810cbcc2a1f04d
ashutosh <ashutoshkumarsinghh17a@gmail.com>
2025-07-16 19:17:36 +0530

    learning about default argument in function

--- function.py
+++ function.py
@@ -9,9 +9,17 @@
 # func1(1,2,3)
 
 # use of return in function
-def func2(a,b,c):
-    average=(a+b+c)/3
-    return average #now its return a value which can be stored in any variable
+# def func2(a,b,c):
+#     average=(a+b+c)/3
+#     return average #now its return a value which can be stored in any variable
 
-a=func2(3,4,5)
-print(a)
+# a=func2(3,4,5)
+# print(a)
+
+
+# default argument in functions
+def func3(name, ending="Thank you"):
+    print(f"good day {name} , {ending}")
+func3("ashu") #Without giving second argumrnt which is by default in function already
+
+func3("ashu","how are you") #giving second argumrnt which is by default in function already
