a692374a1de3833140affc8ed2b1ca69a173deb2
ashutosh <ashutoshkumarsinghh17a@gmail.com>
2025-07-16 19:11:37 +0530

    learning return use in functions

--- function.py
+++ function.py
@@ -1,9 +1,17 @@
 # functions in python
 
-# simple function example
+# function defination
 # function of average
-def func1(a,b,c):
-    print(f"AVERAGE OF NO. ARE {(a+b+c)/3}")
+# def func1(a,b,c):
+#     print(f"AVERAGE OF NO. ARE {(a+b+c)/3}")
 
-# calling a function
-func1(1,2,3)
+# # calling a function
+# func1(1,2,3)
+
+# use of return in function
+def func2(a,b,c):
+    average=(a+b+c)/3
+    return average #now its return a value which can be stored in any variable
+
+a=func2(3,4,5)
+print(a)
