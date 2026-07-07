a90c95ab2b1a69460a3db1bdaebd8375d9d34aa4
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2026-07-07 22:41:55 +0530

    Add Level1pt3Loops.py with exercises for loops, multiplication tables, sum, and factorial

---
+++ new phase/Level1pt3Loops.py
@@ -0,0 +1,72 @@
+# Q19 Print numbers 1 to 100
+
+# Q20 Print 100 to 1
+
+# for i in range(1,101):
+#     print(i)
+# for i in range(100,0,-1):
+#     print(i)
+
+# Q22 Print odd numbers  1 to 100
+# count = 0
+# for i in range(1, 100):
+#     if i % 2 != 0:
+#         print(i)
+#         count += 1
+# print("count = ",count)
+
+# Q23
+
+# Print multiplication table of a number.
+
+# Example
+
+# Input
+
+# 5
+
+# Output
+
+# 5 × 1 = 5
+
+# 5 × 2 = 10
+
+# ...
+
+# 5 × 10 = 50
+
+# num = int(input("enter no. : "))
+
+# for i in range(1, 11):
+#     print(f"{num} x {i} = {num*i}")
+
+
+# Q24
+
+# Find sum of
+
+# 1 to N
+
+# N = int(input("Enter nth no. : "))
+# sum = 0
+# for i in range(1, N+1):
+#     sum += i
+# print(f"Sum {N}th numbers are : {sum}")
+
+
+# Q25
+
+# Find factorial of a number.
+
+# Example
+
+# 5
+
+# 120
+
+
+# N = int(input("Enter nth no. : "))
+# fact = 1
+# for i in range(N, 0, -1):
+#     fact *= i
+# print(f"Factorial of number {N} is : {fact}")
