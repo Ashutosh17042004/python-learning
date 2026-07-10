7269688a9706a601e746c2d10f30418599c73f0d
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2026-07-10 22:23:51 +0530

    Add login system with 3 attempts

---
+++ new phase/Level1Challenge.py
@@ -0,0 +1,32 @@
+# Challenge 1
+
+# Create a Login System with 3 attempts.
+
+# this is my way to solve this problem
+username_in_db = "ashusingh17"
+password_in_db = 12345
+
+
+def invalid_user(count):
+    if count == 0:
+        print("Trail end!!!")
+    if count > 0:
+        print(f"Trail left {count}")
+        print("Invalid Username or Password\nTry Again")
+        login(count)
+
+
+def login(trail):
+    username = input("Enter username : ")
+    password = int(input("Enter password : "))
+
+    if username == username_in_db and password == password_in_db:
+        print("login sucessfully !!!")
+
+    if username != username_in_db or password != password_in_db:
+        trail -= 1
+        invalid_user(trail)
+
+
+trail = 3
+login(trail)
--- new phase/tempCodeRunnerFile.py
+++ new phase/tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-
-def print_inverted_triangle(number):
-    for i in range(number, 0, -1):
-        print("*" * i)
-
-
-print_inverted_triangle(5)
+ashusingh1701
