97178dbaf8e11a7bbf3a5cc7aac0db7a55b07e25
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2026-07-12 22:54:12 +0530

    Add functionality to input, sort, and display highest and lowest marks from a list

---
+++ new phase level 2/List/Senior Developer Questions/ques5.py
@@ -0,0 +1,32 @@
+"""Q17. Highest and Lowest Marks
+
+Take 10 marks.
+
+Store in a list.
+
+Sort.
+
+Print
+
+Highest
+Lowest
+
+⚠ Don't use max() or min().
+
+Use sorting."""
+
+marks_list = []
+
+for i in range(10):
+    marks = int(input(f"Enter subject {i+1} marks : "))
+    marks_list.append(marks)
+
+lowest_marks = marks_list[0]
+highest_marks = marks_list[0]
+for marks in marks_list:
+    if lowest_marks >= marks:
+        lowest_marks = marks
+    if highest_marks <= marks:
+        highest_marks = marks
+print(f"lowest marks : {lowest_marks}")
+print(f"Highest marks : {highest_marks}")
