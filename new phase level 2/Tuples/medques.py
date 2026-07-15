5976514312e4e1dc31bf029bf8f1b9cc7fc758e9
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2026-07-15 10:14:38 +0530

    Add coordinate distance calculation example using tuple unpacking

--- new phase level 2/Tuples/medques.py
+++ new phase level 2/Tuples/medques.py
@@ -33,3 +33,36 @@ Lowest marks : {lowest}
 Number of times 90 appears : {count_90}
 Index of first 75 : {index_75}
 """)
+
+
+"""Q5 🟡 Coordinate Distance
+
+Store two points
+
+point1 = (2, 4)
+
+point2 = (8, 10)
+
+Print
+
+Difference in X
+
+Difference in Y
+
+No formulas.
+
+Just tuple unpacking."""
+
+
+point1 = (2, 4)
+point2 = (8, 10)
+
+x1, y1 = point1
+x2, y2 = point2
+
+print(f"""
+Difference in X :{abs(x1-x2)}
+Difference in Y :{abs(y1-y2)}
+""")
+
+
