a447b6c7dc57e2e416a649a983c373060034ef6a
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2026-07-15 10:36:42 +0530

    Add student ranking example using tuples

---
+++ new phase level 2/Tuples/devques2.py
@@ -0,0 +1,21 @@
+'''Q8 🔴 Student Ranking
+
+Create
+
+students = (
+    ("Ashu", 95),
+    ("Rahul", 88),
+    ("Aman", 91),
+    ("Riya", 99),
+    ("Sumit", 83)
+)
+
+Print
+
+Topper : Riya
+
+Marks : 99
+
+⚠ Don't use max().
+
+Use loops only.'''
