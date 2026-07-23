495e4c56621c82685bd800c32fd4ba2d4f01ae30
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2026-07-23 20:22:55 +0530

    Refactor character frequency counter implementation for clarity and efficiency

--- new phase level 2/Strings/PART A/ques8.py
+++ new phase level 2/Strings/PART A/ques8.py
@@ -1,5 +1,6 @@
 """
 Q8 ⭐⭐⭐⭐⭐ (Character Frequency Counter)
+string = "programming"
 
 Without using:
 ❌ count()
@@ -9,24 +10,40 @@ Without using:
 """
 
 string = "programming"
+list = []
+for ch in string:
+    list.append(ch)
+# print(list)
 
-for i in range(len(string)):
+list2 = []
 
-    # Check if this character has already appeared before
-    already_processed = False
-
-    for j in range(i):
-        if string[i] == string[j]:
-            already_processed = True
-            break
-
-    if already_processed:
+for ch in list:
+    if ch in list2:
         continue
+    else:
+        list2.append(ch)
+        count = list.count(ch)
+        print(f"{ch} -> {count}")
 
-    # Count occurrences
-    count = 0
-    for ch in string:
-        if ch == string[i]:
-            count += 1
 
-    print(f"{string[i]} -> {count}")
+# for i in range(len(string)):
+
+#     # Check if this character has already appeared before
+#     already_processed = False
+
+#     for j in range(i):
+#         if string[i] == string[j]:
+#             already_processed = True
+#             break
+
+#     if already_processed:
+#         continue
+
+
+#     # Count occurrences
+#     count = 0
+#     for ch in string:
+#         if ch == string[i]:
+#             count += 1
+
+#     print(f"{string[i]} -> {count}")
