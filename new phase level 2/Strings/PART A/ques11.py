aba8c9df2e98f37ef97997829805756e2611f2f4
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2026-08-01 22:47:45 +0530

    Add implementation for longest unique substring algorithm

---
+++ new phase level 2/Strings/PART A/ques11.py
@@ -0,0 +1,54 @@
+# Q11 ⭐⭐⭐⭐⭐⭐ (Longest Unique Substring)
+
+# Input
+
+# abcabcbb
+
+# Output
+
+# Longest Unique Substring : abc
+
+# Length : 3
+
+# ----------------------------------
+
+# Input
+
+# bbbb
+
+# Output
+
+# Longest Unique Substring : b
+
+# Length : 1
+
+
+string = "abcabcbb"
+
+longest_substring = ""
+
+for i in range(len(string)):
+
+    current_substring = ""
+
+    for j in range(i, len(string)):
+
+        duplicate = False
+
+        for ch in current_substring:
+            if ch == string[j]:
+                duplicate = True
+                break
+
+        if duplicate:
+            break
+
+        current_substring += string[j]
+
+    if len(current_substring) > len(longest_substring):
+        longest_substring = current_substring
+
+print(f"""
+Longest Unique Substring : {longest_substring}
+Length : {len(longest_substring)}
+""")
